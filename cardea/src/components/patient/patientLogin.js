b0d2aa5a8cb4672c308837d7c1ad9f3e03aecddf
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-21 21:08:41 -0500

    adds my patientLogin with handleChange and preventDefault that returns a div with the form including the inputs I need

---
+++ cardea/src/components/patient/patientLogin.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+// import {withFormik, Form, Field} from "formik";
+// import * as Yup from "yup";
+
+const patientLogin =() =>{
+    const [patient, setPatient] = useState([]);
+
+    const handleChange = e => {
+        setPatient()
+    }
+
+    const login = e => {
+        e.preventDefault();
+        
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={login}>
+                <input
+                    type='text'
+                    name='email'
+                    value={props.email}
+                    onChange={handleChange}
+                    placeholder='User Email'
+                    label='email'
+                />
+
+                <input
+                    type='text'
+                    name='password'
+                    value={props.password}
+                    onChange={handleChange}
+                    placeholder='User password'
+                    label='password'
+                />
+                <button>Login</button>
+            </form>
+            
+        </div>
+    )
+};
+
+export default patientLogin;

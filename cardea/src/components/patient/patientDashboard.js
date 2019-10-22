e80a58a9717aa72923ecf359b16a66e1a878280b
seanaleid <seanaleid@gmail.com>
2019-10-21 22:14:34 -0400

    Adds code to the patientDashboard.js

---
+++ cardea/src/components/patient/patientDashboard.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+// imports 
+
+
+// component
+export const PatientDashboard = () => {
+    //console.log(props)
+
+    return (
+        <div>
+            <div className="header">
+                <h1>The PatientDashboard header goes here.</h1>
+            </div>
+            <div className="body">
+                <h2>The PatientDashboard body goes here.</h2>
+            </div>
+            <div className="footer">
+                <h2>The PatientDashboard footer goes here.</h2>
+            </div>
+        </div>
+    )
+}
+
+//export
+export default PatientDashboard

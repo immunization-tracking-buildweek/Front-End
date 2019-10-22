c799af696d85f092f3447d036580f0c2f44e69b5
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-22 18:48:05 -0500

    Added some code into patientCard but I have not passed all my props down where they need to be yet but will not take long

--- cardea/src/components/patient/patientCard.js
+++ cardea/src/components/patient/patientCard.js
@@ -5,7 +5,7 @@ const PatientCard = props =>{
     console.log("This is the patient card", props)
     return(
         <div>
-            <h1>Props in Here??</h1>
+            <h1>{props.firstName}" "{props.lastName}</h1>
             <p>Immunization history??</p>
             <h3></h3>
             <button>Add info button?</button>

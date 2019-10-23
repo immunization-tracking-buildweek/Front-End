e375a827a737d69970183056778747f2602e80e1
Zack Bumpous <upwork.ztb@gmail.com>
2019-10-23 12:48:57 -0500

    Adds code into the file inside of the sharedCard directory

---
+++ cardea/src/components/sharedCards/PermissionGetCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//ask Karen about patient ID in return
+const PermissionGetCard = props =>{
+    console.log("This is the MedProfPatientCard", props)
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default PermissionGetCard;

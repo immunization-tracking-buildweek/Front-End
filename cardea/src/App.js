5b2cca71695b046c5199efb357390e9b210ed843
seanaleid <seanaleid@gmail.com>
2019-10-24 12:32:12 -0400

    Adds a comment

--- cardea/src/App.js
+++ cardea/src/App.js
@@ -13,7 +13,8 @@ import MedProfRegister from "./components/medPro/MedProfRegister";
 import PatientDashboard from "./components/patient/PatientDashboard";
 import MedProfDashboard from "./components/medPro/MedProfDashboard";
 
-//imports zack added in just now (4:50 pm CST on 10/23)
+//imports Zack added in just now (4:50 pm CST on 10/23)
+// Sean added an uppercase Z to zack ^^^ to commit
 import AddImmunization from "./components/medPro/AddImmunization";
 import AddPatientForm from "./components/patient/AddPatientForm";
 import PermissionForm from "./components/patient/PermissionForm";
